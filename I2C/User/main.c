8aac6573970aac85bbb65b61fdebd02947db545e
gchh <skring.2008@163.com>
2018-06-13 23:43:54 +0800

    xiugai

--- I2C/User/main.c
+++ I2C/User/main.c
@@ -1,8 +1,26 @@
 #include "main.h"
 
+__IO uint32_t uwTick;
+
+void delay(__IO uint32_t delay_cnt)//delay_cnt in 1ms
+{
+   uint32_t tickstart = uwTick;
+   uint32_t wait = delay_cnt;
+
+   /* Add a period to guarantee minimum wait */
+   if (wait < 0xFFFFFFFF)
+   {
+      wait++;
+   }
+  
+   while((uwTick-tickstart)<wait){}
+}
 
 int main(void)
 {
+	/*Configure the SysTick to have interrupt in 1ms time basis*/
+	SysTick_Config(8000);//使用HSI=8MHz作为系统时钟
+	
 	//LED2-PA5
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;  //打开Port A时钟
 	GPIOA->MODER &= (~GPIO_MODER_MODER5);
@@ -21,11 +39,53 @@ int main(void)
 	GPIOB->OTYPER |= GPIO_OTYPER_OT_6 | GPIO_OTYPER_OT_7;//PB6 PB7开漏输出
 	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR6_0 | GPIO_PUPDR_PUPDR7_0;//PB6 PB7上拉
 	
+	/* Disable the selected I2C peripheral */
 	I2C1->CR1 &= ~I2C_CR1_PE;//先关闭I2C1
+	
+	/*---------------------------- I2Cx TIMINGR Configuration ------------------*/
+  /* Configure I2Cx: Frequency range */
+	I2C1->TIMINGR = I2C_TIMING;
+	
+  /*---------------------------- I2C1 OAR1 Configuration ---------------------*/
+  /* Disable Own Address1 before set the Own Address1 configuration */
+  I2C1->OAR1 &= ~I2C_OAR1_OA1EN;
+	/* Configure I2C1: Own Address1 and ack own address1 mode */
+	/* I2C_ADDRESSINGMODE_10BIT */
+	I2C1->OAR1 = I2C_OAR1_OA1EN | I2C_OAR1_OA1MODE | I2C_ADDRESS;
+	/* I2C_ADDRESSINGMODE_7BIT */
+	//I2C1->OAR1 = I2C_OAR1_OA1EN | I2C_ADDRESS&(0x0fe);
+	
+	/*---------------------------- I2C1 CR2 Configuration ----------------------*/
+  /* Configure I2C1: Addressing Master mode */
+	/* I2C_ADDRESSINGMODE_10BIT */
+	I2C1->CR2 = I2C_CR2_ADD10;
+  /* Enable the AUTOEND by default, and enable NACK (should be disable only during Slave process */
+  I2C1->CR2 |= (I2C_CR2_AUTOEND | I2C_CR2_NACK);
+	
+	/*---------------------------- I2C1 OAR2 Configuration ---------------------*/
+  /* Disable Own Address2 before set the Own Address2 configuration */
+	I2C1->OAR2 &= ~I2C_OAR2_OA2EN;
+  /* Configure I2C1: Dual mode and Own Address2 */
+  //I2C1->OAR2 = (I2C_OAR2_OA2EN | I2C_ADDRESS_2 | (I2C_ADDRESS_2_MSK << 8));	
+	
+  /*---------------------------- I2C1 CR1 Configuration ----------------------*/
+  /* Configure I2C1: Generalcall and NoStretch mode */
+  I2C1->CR1 &= ~(I2C_CR1_GCEN | I2C_CR1_NOSTRETCH);
+	/* Enable the Analog I2C Filter */
+	/* Reset I2C1 ANOFF bit */
+	I2C1->CR1 &= ~(I2C_CR1_ANFOFF);	
+		
+  /* Enable the selected I2C peripheral */
+  I2C1->CR1 |= I2C_CR1_PE;	
+
+	/*##-1- Start the transmission process #####################################*/  
+	/* Timeout is set to 10S */
+	
 	
 	while(1)
 	{
 			GPIOA->ODR ^= GPIO_ODR_5;
+			delay(1000);
 			//SET_LED2;
 			//CLR_SET_LED2_BIT;
 
@@ -34,3 +94,12 @@ int main(void)
 	}
 }
 
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+   uwTick++;
+}
--- I2C/User/main.h
+++ I2C/User/main.h
@@ -1,5 +1,15 @@
 #include "stm32f0xx.h"
 
+#define I2C_ADDRESS        0x30F
+#define I2C_ADDRESS_2      0xFE
+#define I2C_ADDRESS_2_MSK  0//0-7
+
+/* I2C TIMING Register define when I2C clock source is SYSCLK */
+/* I2C TIMING is calculated in case of the I2C Clock source is the SYSCLK = 48 MHz */
+/* This example use TIMING to 0x00A51314 to reach 1 MHz speed (Rise time = 100 ns, Fall time = 100 ns) */
+#define I2C_TIMING      0x00A51314
+
+
 #define SET_LED2 GPIOA->BSRR |= GPIO_BSRR_BS_5
 #define CLR_SET_LED2_BIT GPIOA->BSRR &= ~GPIO_BSRR_BS_5
 
