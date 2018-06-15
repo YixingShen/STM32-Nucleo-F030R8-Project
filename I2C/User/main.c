5f567f4309fa714610bce83e7a561e749426bf6d
gchh <skring.2008@163.com>
2018-06-15 17:28:01 +0800

    xiugai

--- I2C/User/main.c
+++ I2C/User/main.c
@@ -16,6 +16,37 @@ void delay(__IO uint32_t delay_cnt)//delay_cnt in 1ms
    while((uwTick-tickstart)<wait){}
 }
 
+void I2C_peripheral_init(void)
+{
+	//I2C1_SCL-PB6; I2C1_SDA-PB7
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;  //打开Port B时钟
+	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;  //打开I2C1时钟
+	RCC->CFGR3 |= RCC_CFGR3_I2C1SW_SYSCLK;//I2C1时钟源选择SYSCLK
+	GPIOB->MODER |= GPIO_MODER_MODER7_1 | GPIO_MODER_MODER6_1;//PB6 PB7复用功能
+	GPIOB->AFR[0] |= (1<<28) | (1<<24);//PB6-AF1 PB7-AF1
+	GPIOB->OSPEEDR |= GPIO_OSPEEDR_OSPEEDR6 | GPIO_OSPEEDR_OSPEEDR7;//PB6 PB7高速
+	GPIOB->OTYPER |= GPIO_OTYPER_OT_6 | GPIO_OTYPER_OT_7;//PB6 PB7开漏输出
+	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR6_0 | GPIO_PUPDR_PUPDR7_0;//PB6 PB7上拉
+   
+   //复位I2C1
+   RCC->APB1RSTR |= RCC_APB1RSTR_I2C1RST;
+   RCC->APB1RSTR &= ~RCC_APB1RSTR_I2C1RST;   
+   
+	/* Disable the selected I2C peripheral */
+	I2C1->CR1 &= ~I2C_CR1_PE;//先关闭I2C1
+}
+
+void I2C_registers_Configuration(void)
+{
+   //I2C configured in master mode to transmit code
+   /* (1) Timing register value is computed with the AN4235 xls file, fast Mode @400kHz with I2CCLK = 48MHz, rise time = 140ns, fall time = 40ns */
+   /* (2) Periph enable */
+   /* (3) Slave address = 0xA0, write transfer, 1 byte to transmit, autoend */
+   I2C2->TIMINGR = (uint32_t)0x00B01A4B; /* (1) */
+   I2C2->CR1 = I2C_CR1_PE; /* (2) */
+   I2C2->CR2 = I2C_CR2_AUTOEND | (1 << 16) | 0xA0; /* (3) */   
+}
+
 int main(void)
 {
 	/*Configure the SysTick to have interrupt in 1ms time basis*/
@@ -30,22 +61,9 @@ int main(void)
 	//GPIOA->PUPDR |= GPIO_PUPDR_PUPDR5_0; //PA5 PULL-UP
 
 	//I2C1_SCL-PB6; I2C1_SDA-PB7
-	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;  //打开Port B时钟
-	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;  //打开I2C1时钟
-	RCC->CFGR3 |= RCC_CFGR3_I2C1SW_SYSCLK;//I2C1时钟源选择SYSCLK
-	GPIOB->MODER |= GPIO_MODER_MODER7_1 | GPIO_MODER_MODER6_1;//PB6 PB7复用功能
-	GPIOB->AFR[0] |= (1<<28) | (1<<24);//PB6-AF1 PB7-AF1
-	GPIOB->OSPEEDR |= GPIO_OSPEEDR_OSPEEDR6 | GPIO_OSPEEDR_OSPEEDR7;//PB6 PB7高速
-	GPIOB->OTYPER |= GPIO_OTYPER_OT_6 | GPIO_OTYPER_OT_7;//PB6 PB7开漏输出
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR6_0 | GPIO_PUPDR_PUPDR7_0;//PB6 PB7上拉
+	I2C_peripheral_init();//I2C外设初始化：gpio clock等设置
    
-   //复位I2C1
-   RCC->APB1RSTR |= RCC_APB1RSTR_I2C1RST;
-   RCC->APB1RSTR &= ~RCC_APB1RSTR_I2C1RST;
-   
-	
-	/* Disable the selected I2C peripheral */
-	I2C1->CR1 &= ~I2C_CR1_PE;//先关闭I2C1
+
 	
 	/*---------------------------- I2Cx TIMINGR Configuration ------------------*/
   /* Configure I2Cx: Frequency range */
