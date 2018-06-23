dafe4ed3dc0e2410269e5680ecc6e7e01659cb96
gchh <skring.2008@163.com>
2018-06-23 17:30:18 +0800

    xiugai

--- I2C/User/main.c
+++ I2C/User/main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 __IO uint32_t uwTick;
+uint8_t flag_step,rx[3],cnt;
 
 void delay(__IO uint32_t delay_cnt)//delay_cnt in 1ms
 {
@@ -40,21 +41,27 @@ void I2C_peripheral_init(void)
    
 	/* Disable the selected I2C peripheral */
 	I2C1->CR1 &= ~I2C_CR1_PE;//先关闭I2C1
-}
-
-void I2C_registers_Configuration(void)
-{
+   
    //I2C configured in master mode to transmit code
    /* (1) Timing register value is computed with the AN4235 xls file, fast Mode @400kHz with I2CCLK = 48MHz, rise time = 140ns, fall time = 40ns */
    /* (2) Periph enable */
-   /* (3) Slave address = 0xA0, write transfer, 1 byte to transmit, autoend */
    I2C1->TIMINGR = (uint32_t)0x00B01A4B; /* (1) */
    I2C1->CR1 = I2C_CR1_PE; /* (2) */
-   I2C1->CR2 = I2C_CR2_AUTOEND | (1 << 16) | 0xA0; /* (3) */   
-   
+
    /* Enable interrupts */
    I2C1->CR1 |= I2C_CR1_ERRIE | I2C_CR1_TCIE | I2C_CR1_STOPIE | I2C_CR1_NACKIE 
-              | I2C_CR1_ADDRIE | I2C_CR1_RXIE | I2C_CR1_TXIE;
+              | I2C_CR1_ADDRIE | I2C_CR1_RXIE | I2C_CR1_TXIE;  
+}
+
+void I2C_registers_Configuration(uint8_t step)
+{
+   //I2C configured in master mode to transmit code
+   /* (3) Slave address = 0x5A, write transfer, 1 byte to transmit, autoend */
+   if(step==0)I2C1->CR2 = I2C_CR2_RELOAD | (1 << 16) | 0xA0; //I2C_CR2_AUTOEND | (1 << 16) | 0xA0; /* (3) */   
+   
+   //I2C configured in master mode to receive code example
+   /* (3) Slave address = 0x5A, read transfer, 1 byte to receive, autoend */
+   else if(step==1)I2C2->CR2 = I2C_CR2_AUTOEND | (3<<16) | I2C_CR2_RD_WRN | 0xA1; //I2C_CR2_RELOAD | (3<<16) | I2C_CR2_RD_WRN | 0xA1; /* (3) */
    
 }
 
@@ -75,8 +82,10 @@ int main(void)
 	I2C_peripheral_init();//I2C外设初始化：gpio clock等设置
    
    /*---------------------------- I2C1 Configuration ----------------------*/
-   I2C_registers_Configuration();
-	
+   flag_step=0;
+	cnt=0;
+   //I2C_registers_Configuration(flag_step);
+   
 	while(1)
 	{
 			GPIOA->ODR ^= GPIO_ODR_5;
@@ -86,6 +95,8 @@ int main(void)
 
 			//RESET_LED2;
 			//CLR_RESET_LED2_BIT;
+      
+      I2C_registers_Configuration(flag_step);
 	}
 }
 
@@ -98,7 +109,6 @@ void SysTick_Handler(void)
 {
    uwTick++;
 }
-
 
 
 void I2C1_IRQHandler(void)
@@ -129,5 +139,55 @@ void I2C1_IRQHandler(void)
       I2C1->ICR |= I2C_ICR_BERRCF;
    }   
    
-   //
+   //ADDRIE
+   if(I2C1->ISR & I2C_ISR_ADDR)
+   {
+      I2C1->ICR |= I2C_ICR_ADDRCF;
+   }
+   
+   //Check Tx empty
+   if(I2C1->ISR & I2C_ISR_TXE)
+   {
+      I2C1->TXDR = 0x08;
+      I2C1->CR2 |= I2C_CR2_START;
+   }
+   //传输完成
+   if(I2C1->ISR & I2C_ISR_TCR)
+   {
+      I2C1->CR2 &= ~I2C_CR2_START;
+      if(flag_step==0)
+      {
+         flag_step=1;
+         I2C1->CR2 |= (3 << 16);
+         //I2C_registers_Configuration(flag_step);
+      }
+      else 
+      {
+         //I2C1->CR2 |= I2C_CR2_STOP;
+         flag_step=0;
+         I2C1->CR2 |= (1 << 16); 
+         //I2C_registers_Configuration(flag_step);
+      }
+      cnt=0;
+   }
+   
+   //I2C master receiver code
+   if(I2C1->ISR & I2C_ISR_RXNE)
+   {
+      rx[cnt]=I2C1->RXDR;
+      cnt++;
+      //if(cnt==3) I2C1->CR2 |= I2C_CR2_STOP;
+   }
+   
+   //NACKIE
+   if(I2C1->ISR & I2C_ISR_NACKF)
+   {
+      I2C1->ICR |= I2C_ICR_NACKCF;
+   }  
+   //STOPIE  
+   if(I2C1->ISR & I2C_ISR_STOPF)
+   {
+      I2C1->ICR |= I2C_ICR_STOPCF;
+   }     
 }
+
