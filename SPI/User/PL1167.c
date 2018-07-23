74c50a38c064be4356f0aae9d085ffb146a86421
gchh <skring.2008@163.com>
2018-07-23 17:28:18 +0800

    xiugai

---
+++ SPI/User/PL1167.c
@@ -0,0 +1,306 @@
+#include "main.h"
+extern void delay(__IO uint32_t delay_cnt);
+
+unsigned int address_code;
+#ifdef Send_Mode
+unsigned char go_sleep,fun_code,data_code,flag_RFsend;
+#endif
+#ifdef Receive_Mode
+unsigned char fun_code_rec,data_code_rec;
+unsigned int address_code_rec;
+#endif
+
+unsigned char DAT[gLength+1]={0,};          // First Byte:HW length. Runtime code must set this first byte accordingly.
+unsigned char gReg7_high,gReg7_low;    // 寄存器7读写暂存变量
+unsigned char Auto_Ack_Select=0;            // 自动应答开关
+unsigned int tempreg;
+unsigned char timeout_cnt;
+_Bool update;
+unsigned char tx[5],rx[5];
+unsigned char send_size,receive_size;
+//-----------------------------------------------------------------------------
+// Function PROTOTYPES
+//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+// 微秒延时
+//------------------------------------------------------------------------------------
+void msec(unsigned int x)
+{
+        delay(x);
+}
+
+void pdelay (unsigned char t)
+{
+        while(t!=0)
+                t--;
+}
+
+void delay_msec(unsigned int x)
+{
+        unsigned char i,j;
+        while(x--)
+        {
+                i=11,j=50;
+                while(i--)
+                        while(j--) ;
+        }
+}
+
+
+//-----------------------------------------------------------------------------
+// SPI 16位寄存器写操作
+//-----------------------------------------------------------------------------
+void Reg_write16(unsigned char addr, unsigned char v1, unsigned char v2)
+{
+        //等待上一次发送完
+        while((SPI2->SR & SPI_SR_FTLVL)!=0);
+        while((SPI2->SR & SPI_SR_BSY)!=0);
+
+        tx[0] = addr;
+        tx[1] = v1;
+        tx[2] = v2;
+        send_size=3; 
+
+        SPI2->CR2 |= SPI_CR2_TXEIE;
+   
+        if(addr < 0x20)
+                pdelay(RF_GAP); 
+}
+//-----------------------------------------------------------------------------
+// SPI 16位寄存器读操作
+//-----------------------------------------------------------------------------
+unsigned int Reg_read16(unsigned char addr)
+{
+        //unsigned char clr;
+        unsigned int value =0;
+        //等待上一次发送完
+        while((SPI2->SR & SPI_SR_FTLVL)!=0);
+        while((SPI2->SR & SPI_SR_BSY)!=0);   
+        tx[0] = addr | REG_RD;
+        send_size=1; 
+        SPI2->CR2 |= SPI_CR2_TXEIE;
+   
+        if(addr < 0x20)
+        {
+                pdelay(RF_GAP);
+        }
+        //等待发送完毕
+        while((SPI2->CR2 & SPI_CR2_TXEIE)!=0);
+        while((SPI2->SR & SPI_SR_FTLVL)!=0);
+        while((SPI2->SR & SPI_SR_BSY)!=0);           
+        
+        //清RXFIFO
+        //while((SPI2->SR & SPI_SR_FRLVL)!=0)clr=SPI2->DR;
+        //接收
+        SPI2->CR2 |= SPI_CR2_RXNEIE;
+        receive_size=2;
+        //等待接收完
+        while((SPI2->CR2 & SPI_CR2_RXNEIE)!=0);
+        value = rx[0];
+        value<<=8;
+        value |= rx[1];
+
+        return value;
+}
+//获取寄存器 7 的值
+void GetReg7FromChip(void)
+{
+        unsigned int temp;
+
+        temp= Reg_read16(0x07);
+        gReg7_high= (unsigned char) ((temp & 0xff00) >> 8);
+        gReg7_low = (unsigned char) (temp & 0x00ff);
+}
+//-----------------------------------------------------------------------------
+// RF 初始化
+//-----------------------------------------------------------------------------
+void PL1167_Init(void)
+{
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;  //打开Port A时钟
+	GPIOA->MODER &= ~GPIO_MODER_MODER1;//PA1输入
+	GPIOA->MODER |= GPIO_MODER_MODER0_0;//PA0输出
+	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT_1 | GPIO_OTYPER_OT_0);//PA1 PA0推挽输出
+	//GPIOA->PUPDR |= GPIO_PUPDR_PUPDR1_1 | GPIO_PUPDR_PUPDR0_1;//PA1 PA0下拉
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR1_0 | GPIO_PUPDR_PUPDR0_0;//PA1 PA0上拉
+   
+        RFRST_H;
+        msec(10);
+        RFRST_L;
+        msec(10);
+        RFRST_H;               //Enable PL1167
+        msec(10);               //delay 5ms to let chip stable
+        Reg_write16(0x00,0x6f,0xe0);
+        Reg_write16(0x01,0x56,0x81);
+        Reg_write16(0x02,0x66,0x17);
+        Reg_write16(0x04,0x9c,0xc9);
+        Reg_write16(0x05,0x66,0x37);
+        Reg_write16(0x07,0x00,0x30);
+        Reg_write16(0x08,0x6c,0x90);
+        Reg_write16(0x09,0x18,0x40);
+        Reg_write16(0x0a,0x7f,0xfd);
+        Reg_write16(0x0b,0x00,0x08);
+        Reg_write16(0x0c,0x00,0x00);
+        Reg_write16(0x0d,0x48,0xbd);
+        Reg_write16(0x16,0x00,0xff);
+        Reg_write16(0x17,0x80,0x05);
+        Reg_write16(0x18,0x00,0x67);
+        Reg_write16(0x19,0x16,0x59);
+        Reg_write16(0x1a,0x19,0xe0);
+        Reg_write16(0x1b,0x13,0x00);
+        Reg_write16(0x1c,0x18,0x00);
+        Reg_write16(0x20,0x48,0x00);
+        Reg_write16(0x21,0x3f,0xc7);
+        Reg_write16(0x22,0x20,0x00);
+        Reg_write16(0x23,0x03,0x80);
+//Begin Syncword					//不同的Syncword可以用以区分不同的设备
+        Reg_write16(0x24,0x42,0x31);
+        Reg_write16(0x25,0x86,0x75);
+        Reg_write16(0x26,0x9a,0x0b);
+        Reg_write16(0x27,0xde,0xcf);
+//End syncword
+        Reg_write16(0x28,0x44,0x01);
+        Reg_write16(0x29,0xb0,0x00);
+        Reg_write16(0x2a,0xfd,0xb0);
+        Reg_write16(0x2b,0x00,0x0f);
+        msec(100);              //delay 100ms to let chip for operation
+        GetReg7FromChip();
+}
+void RF_EnterSleep(void)
+{
+        Reg_write16(0x23,0x43,0x80);
+}
+
+//*================================================================================
+//*Fuction  : TX packet via FIFO
+//*Parameter: packet data and length of payload sent
+//return    : none
+//*================================================================================
+void TX_packet(unsigned char *ptr,unsigned char bytes) //only tx loop
+{
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////Because MCU is too fast,MCU write 60 bytes every time instead of 64 bytes lest the FIFO isn't over written
+///////Pls refer to register56[15:12]:TX_FIFO_threshold
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        unsigned char j;
+
+        Reg_write16(0x34,0x80,0);// reset TX FIFO point
+
+        //等待上一次发送完
+        while((SPI2->SR & SPI_SR_FTLVL)!=0);
+        while((SPI2->SR & SPI_SR_BSY)!=0);   
+        tx[0] = 0x32;     //Fill data to FIFO;
+        send_size=1; 
+        SPI2->CR2 |= SPI_CR2_TXEIE;
+        
+        //等待发送完
+        while((SPI2->CR2 & SPI_CR2_TXEIE)!=0);
+        while((SPI2->SR & SPI_SR_FTLVL)!=0);
+        while((SPI2->SR & SPI_SR_BSY)!=0);   
+   
+        for(j=0; j<(bytes+1); j++)
+        {
+                tx[j] = *ptr++;
+        }
+        SPI2->CR2 |= SPI_CR2_TXEIE;
+        
+        Reg_write16(0x07, (0x00|0x01), gReg7_low&0x7f);//TX Enable
+//	pdelay(10);
+        //refresh_watchdog;
+        while(PKT_IS_LOW) {LED2_ON;}
+				LED2_OFF;
+				
+        //refresh_watchdog;
+        Reg_write16(0x07, (0x00&0xfe), gReg7_low&0x7f); //TX Disable
+}
+//*=============================================================================================================
+//* PS: First byte received may be wrong, so FIFO_flag may not turn high as expectation
+//* Function: RX packet from FIFO
+//* Parameter: two;EnableTimeout=1, enable timeout function; bytes, how much bytes you wanted to receive
+//* return   :
+//*=============================================================================================================
+void RX_packet(void)
+{
+        unsigned char j;
+        unsigned char timeout=0;
+
+        Reg_write16(0x34,0,0x80);                   // reset RX FIFO point
+        Reg_write16(0x07, 0x00, (gReg7_low|0x80));  //enter RX mode
+
+        timeout_cnt=0;//Set timeout;
+        while (PKT_IS_LOW)
+        {
+						msec(1);
+						timeout_cnt++;
+						if(timeout_cnt>=10)
+						{
+								timeout=1;
+								goto time_out;
+						}
+						else timeout=0;
+        };
+
+Had_Rec:
+        //等待上一次发送完
+        while((SPI2->SR & SPI_SR_FTLVL)!=0);
+        while((SPI2->SR & SPI_SR_BSY)!=0);   
+        tx[0] = 0x32 | REG_RD;     //Read FIFO datas
+        send_size=1; 
+        SPI2->CR2 |= SPI_CR2_TXEIE;        
+        
+        //等待发送完
+        while((SPI2->CR2 & SPI_CR2_TXEIE)!=0);
+        while((SPI2->SR & SPI_SR_FTLVL)!=0);
+        while((SPI2->SR & SPI_SR_BSY)!=0);   
+        
+        //接收
+        SPI2->CR2 |= SPI_CR2_RXNEIE;
+        receive_size=2;
+        //等待接收完
+        while((SPI2->CR2 & SPI_CR2_RXNEIE)!=0);        
+        DAT[0] = rx[0];
+        if(DAT[0]>20) DAT[0]=20;            //Maybe first Byte is error;
+        for(j=1; j<(DAT[0]+1); j++)             // SPIF0 take 200us to read/write 33 bytes
+                DAT[j] = rx[j];
+
+time_out:
+        if(!timeout)                        //Check if the recevied data is valid;
+        {
+                delay_msec(10);
+        }
+        else update =0;
+}
+#ifdef Send_Mode
+void Send_DATA(unsigned char fun_code, unsigned char data_code)
+{
+        unsigned char j;
+        if(go_sleep==1) PL1167_Init();
+        go_sleep=0;
+
+        //4字节：2字节地址码+1字节功能+1字节数据
+        DAT[0]=4;
+        DAT[1]=address_code>>8;
+        DAT[2]=address_code;
+        DAT[3]=fun_code;    //0风扇控制，1音量控制
+        DAT[4]=data_code;    //风扇或音量挡位
+
+        TX_packet(DAT, DAT[0]);
+
+        for(j=0; j<(DAT[0]+1); j++)
+        {
+                DAT[j]=0;
+        }
+        //delay_msec(20);
+}
+#endif
+
+#ifdef Receive_Mode
+void Receive_DATA(void)
+{
+        RX_packet();
+        address_code_rec=DAT[1]<<8;
+        address_code_rec+=DAT[2];
+        fun_code_rec=DAT[3];//功能码
+        data_code_rec=DAT[4];//数据
+}
+#endif
--- SPI/User/main.c
+++ SPI/User/main.c
@@ -1,9 +1,6 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 __IO uint32_t uwTick;
-uint8_t rx[10],rx_cnt,tx[10],tx_cnt,trans_recv,tx_size,flag_rx,led_light,tx_step;
 
 void delay(__IO uint32_t delay_cnt)//delay_cnt in 1ms
 {
@@ -29,15 +26,23 @@ void LED_init(void)
 	//GPIOA->PUPDR |= GPIO_PUPDR_PUPDR5_0; //PA5 PULL-UP   
 }
 
+void Button_init(void)
+{
+	//USER_BUTTON-PC13
+	RCC->AHBENR |= RCC_AHBENR_GPIOCEN; //打开PORT C时钟
+	GPIOC->MODER &= (~GPIO_MODER_MODER13); //PC13设为输入
+}
+
 void SPI_init(void)
 {
+#if 0
    /******************************************************************
    SPI1_NSS:  PA4
    SPI1_SCK:  PB3
    SPI1_MISO: PB4
    SPI1_MOSI: PB5
    *******************************************************************/
-   RCC->AHBENR |= RCC_AHBENR_GPIOAEN;  //打开Port A时钟S
+   RCC->AHBENR |= RCC_AHBENR_GPIOAEN;  //打开Port A时钟
 	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;  //打开Port B时钟
 	RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;  //打开SPI1时钟
 	GPIOA->MODER |= GPIO_MODER_MODER4_1;//PA4复用功能
@@ -51,7 +56,7 @@ void SPI_init(void)
 	GPIOB->OSPEEDR |= GPIO_OSPEEDR_OSPEEDR5 | GPIO_OSPEEDR_OSPEEDR4 | GPIO_OSPEEDR_OSPEEDR3;//PB3 PB4 PB5高速
    GPIOB->OTYPER &= ~(GPIO_OTYPER_OT_5 | GPIO_OTYPER_OT_4 | GPIO_OTYPER_OT_3);//PB3 PB4 PB5推挽输出
 	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR5_1 | GPIO_PUPDR_PUPDR4_1 |GPIO_PUPDR_PUPDR3_1;//PB3 PB4 PB5下拉
-   
+
    //复位SPI1
    RCC->APB2RSTR |= RCC_APB2RSTR_SPI1RST;
    RCC->APB2RSTR &= ~RCC_APB2RSTR_SPI1RST;   
@@ -87,58 +92,116 @@ void SPI_init(void)
 	NVIC_SetPriority(SPI1_IRQn, 0);
 	//Enable Interrupt on SPI1
 	NVIC_EnableIRQ(SPI1_IRQn);   
-}
+#else
+   /******************************************************************
+   SPI2_NSS:  PB12
+   SPI2_SCK:  PB13
+   SPI2_MISO: PB14
+   SPI2_MOSI: PB15
+   *******************************************************************/
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;  //打开Port B时钟
+	RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;  //打开SPI2时钟
 
-void commu(void)
-{
-   if(trans_recv==0)//发送
-   {
-      trans_recv=1;
-      
-      if(tx_step<2)
-      {
-         if(tx_step==0)sprintf(tx,"Ready\r\n");
+   GPIOB->MODER |= GPIO_MODER_MODER12_1 | GPIO_MODER_MODER13_1 | GPIO_MODER_MODER14_1 | GPIO_MODER_MODER15_1;//PB12 PB13 PB14 PB15复用功能
+   GPIOB->AFR[1] &= 0x0000FFFF;//PB15-AF0 PB14-AF0 PB13-AF0 PB12-AF0
+	GPIOB->OSPEEDR |= GPIO_OSPEEDR_OSPEEDR15 | GPIO_OSPEEDR_OSPEEDR14 | GPIO_OSPEEDR_OSPEEDR13 | GPIO_OSPEEDR_OSPEEDR12;//PB12 PB13 PB14 PB15高速
+   GPIOB->OTYPER &= ~(GPIO_OTYPER_OT_15 | GPIO_OTYPER_OT_14 | GPIO_OTYPER_OT_13 | GPIO_OTYPER_OT_12);//PB12 PB13 PB14 PB15推挽输出
+	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR15_1 | GPIO_PUPDR_PUPDR14_1 |GPIO_PUPDR_PUPDR13_1 |GPIO_PUPDR_PUPDR12_1;//PB12 PB13 PB14 PB15下拉
 
-				 tx_size=strlen(tx);//strlen() 函数计算的是字符串的实际长度，遇到第一个'\0'结束，不包括结束字符"\0"。
-				 
-				 USART1->TDR = tx[0];
-			}
-			else tx_size=0;
-			tx_step=2;
-   }
-   else//接收
-   {
-      if(flag_rx==1)
-      {
-				 tx_step=1;
-				 flag_rx=0;
-				 trans_recv=0;
-         if(strncmp(rx,"LED:ON",sizeof("LED:ON")-1)==0)led_light=1;
-         else if(strncmp(rx,"LED:OFF",sizeof("LED:OFF")-1)==0)led_light=0;
-         strcpy(tx,rx);
-				 //strcpy()把rx所指的由NULL结束的字符串复制到tx所指的数组中,返回指向tx字符串的起始地址。
-				 //所以rx[9]必须为0
-      }
-   }
+   //复位SPI2
+   RCC->APB1RSTR |= RCC_APB1RSTR_SPI2RST;
+   RCC->APB1RSTR &= ~RCC_APB1RSTR_SPI2RST;   
+   
+   /******************************************
+    空闲状态SCK低电平，第一个SCK边沿采样数据；
+    主模式；
+    Baud Rate：fpclk/2=4MBits/s，fpclk=fhclk=fsysclk=8MHz；
+    MSB在前；
+    NSS硬件管理；
+    2线全双工；
+    不使用CRC；
+    ******************************************/
+   SPI2->CR1 = SPI_CR1_MSTR;
+   /******************************************
+    不使用TX和RX DMA；
+    NSS输出使能；
+    不使用NSS脉冲模式；
+    使用Motorola模式；
+    错误中断使能；
+    RXNE中断使能；
+    TXE中断使能；
+    数据长度：8bit；
+    接收阈值：8bit；
+    ******************************************/   
+   SPI2->CR2 = SPI_CR2_FRXTH | SPI_CR2_DS_2 | SPI_CR2_DS_1 | SPI_CR2_DS_0 | SPI_CR2_SSOE;
+   //          | SPI_CR2_TXEIE | SPI_CR2_RXNEIE | SPI_CR2_ERRIE;
+   //使能SPI2
+   SPI2->CR1 |= SPI_CR1_SPE;
+
+	/* Configure NVIC for SPI2 Interrupt */
+	//set SPI2 Interrupt to the lowest priority
+	NVIC_SetPriority(SPI2_IRQn, 0);
+	//Enable Interrupt on SPI1
+	NVIC_EnableIRQ(SPI2_IRQn);   
+#endif
 }
 
 int main(void)
 {
+   uint8_t key_press_cnt;
+   
 	/*Configure the SysTick to have interrupt in 1ms time basis*/
 	SysTick_Config(8000);//使用HSI=8MHz作为系统时钟
 	
 	//LED2-PA5
    LED_init();
+   Button_init();
+   SPI_init();
 
+   address_code=ADD_INI;
+	fun_code=0;
+	data_code=0;
+	PL1167_Init();
+   
 	while(1)
 	{
-			//GPIOA->ODR ^= GPIO_ODR_5;
-			commu();
+		if(KEY_PRESS)//按键按下
+		{
+			if(key_press_cnt>3)
+			{
+				key_press_cnt=0;
+				flag_RFsend=1;
+            if(data_code>8)
+				{
+					data_code=0;
+					if(fun_code==1)fun_code=0;
+               else fun_code=1;
+				}
+				else data_code++;
+			}
+			else key_press_cnt++;
+		}
+		else key_press_cnt=0;		
+		
+		
+#ifdef Send_Mode//只发
+		if(flag_RFsend)
+		{
+			flag_RFsend=0;
+			Send_DATA(fun_code,data_code);
+		}
+		else if(0)//(go_sleep==0)//不发送，进入休眠
+		{
+			//如果休眠，在退出休眠时要初始化，造成LED闪
+			go_sleep=1;
+			RF_EnterSleep();
+		} 
+#endif
+#ifdef Receive_Mode//只收
+		Receive_DATA();   
+#endif		
       
-			if(led_light==1)GPIOA->ODR |= GPIO_ODR_5;
-			else GPIOA->ODR &= ~GPIO_ODR_5;
-
-		  delay(20);
+      delay(20);
 	}
 }
 
@@ -151,7 +214,7 @@ void SysTick_Handler(void)
 {
    uwTick++;
 }
-
+#if 0
 /**
   * @brief  This function is executed in case of error occurrence.
   * @param  None
@@ -162,11 +225,13 @@ static void Error_Handler(void)
   while(1)
   {
     /* Toggle LED2 */
-    GPIOA->ODR ^= GPIO_ODR_5;
+    LED2_Toggle;
     delay(1000);
   }
 }
-
+#endif
+uint8_t sent_cnt,receive_cnt;
+#if 0
 void SPI1_IRQHandler(void)
 {
    int8_t i,error_code=0;
@@ -223,3 +288,76 @@ void SPI1_IRQHandler(void)
     return;
   }   
 }
+#else
+void SPI2_IRQHandler(void)
+{
+   //int8_t i,error_code=0;
+   
+   /* SPI in mode Receiver ----------------------------------------------------*/
+   if (((SPI2->SR & SPI_SR_OVR) == RESET) &&
+      ((SPI2->SR  & SPI_SR_RXNE) != RESET) && ((SPI2->CR2  & SPI_CR2_RXNEIE) != RESET))
+   {
+      rx[receive_cnt] = SPI2->DR;
+      receive_cnt++;
+      if(receive_cnt>=receive_size)receive_cnt=0;
+      else 
+      {
+         receive_cnt=0;
+         SPI2->CR2 &= ~SPI_CR2_RXNEIE;
+      }
+      return;
+   }
+
+   /* SPI in mode Transmitter -------------------------------------------------*/
+   if (((SPI2->SR & SPI_SR_TXE) != RESET) && ((SPI2->CR2 & SPI_CR2_TXEIE) != RESET))
+   {
+      SPI2->DR = tx[sent_cnt];
+      if(sent_cnt<send_size-1)sent_cnt++;
+      else 
+      {
+         sent_cnt=0;
+         SPI2->CR2 &= ~SPI_CR2_TXEIE;
+      }
+   }
+#if 0
+   /* SPI in Error Treatment --------------------------------------------------*/
+   if (((SPI2->SR & (SPI_SR_MODF | SPI_SR_OVR | SPI_SR_FRE)) != RESET) && ((SPI2->CR2 & SPI_CR2_ERRIE) != RESET))
+   {
+      /* SPI Overrun error interrupt occurred ----------------------------------*/
+      if ((SPI2->SR & SPI_SR_OVR) != RESET)
+      {
+         error_code=1;
+         i = SPI2->DR;
+         i = SPI2->SR;
+         return;
+      }
+
+      /* SPI Mode Fault error interrupt occurred -------------------------------*/
+      if ((SPI2->SR & SPI_SR_MODF) != RESET)
+      {
+         error_code=2;
+         i = SPI2->SR;
+         SPI2->CR1 &= ~SPI_CR1_SPE;
+      }
+
+      /* SPI Frame error interrupt occurred ------------------------------------*/
+      if ((SPI2->SR & SPI_SR_FRE) != RESET)
+      {
+         error_code=3;
+         i = SPI2->SR;
+      }
+
+      if (error_code != 0)
+      {
+         if(i!=0)i=0;
+         /* Disable all interrupts */
+         SPI2->CR2 &= ~(SPI_CR2_RXNEIE | SPI_CR2_TXEIE | SPI_CR2_ERRIE);
+         /* Call user error callback */
+         Error_Handler();
+      }
+      return;
+   }   
+#endif
+}
+#endif
+
